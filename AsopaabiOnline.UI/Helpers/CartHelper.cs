c1361cc8cffc8abc3a5e506e8acde97f67194221
Rocio Ramirez <chio28.rr@gmail.com>
2021-02-08 09:47:08 -0600

    pedidos

--- AsopaabiOnline.UI/Helpers/CartHelper.cs
+++ AsopaabiOnline.UI/Helpers/CartHelper.cs
@@ -9,7 +9,7 @@ namespace AsopaabiOnline.UI.Helpers
         public static void SetObjectAsJson(this ISession session, string key, object value)
         {
             session.SetString(key, JsonConvert.SerializeObject(value, new JsonSerializerSettings
-            { PreserveReferencesHandling = PreserveReferencesHandling.All }));
+            { PreserveReferencesHandling.All = PreserveReferencesHandling.All }));
         }
 
         public static T GetObjectFromJson<T>(this ISession session, string key)
